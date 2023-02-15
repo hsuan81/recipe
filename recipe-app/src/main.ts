3bf89d8988bb6d5dfe5391c50c39491031b1e9fc
Hsuanyin <61041593+hsuan81@users.noreply.github.com>
2023-02-15 17:57:56 +0800

    modified procfile

--- recipe-app/src/main.ts
+++ recipe-app/src/main.ts
@@ -5,7 +5,7 @@ import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false })
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
-  await app.listen(3333)
+  await app.listen(process.env.PORT ||3333)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
