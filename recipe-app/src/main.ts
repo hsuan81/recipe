a5d96a0282cd4eedbff71376e7d356d1f49c3186
Hsuanyin <61041593+hsuan81@users.noreply.github.com>
2023-02-20 22:17:35 +0800

    fixed script commands

--- recipe-app/src/main.ts
+++ recipe-app/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress'
+// import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false })
-  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
-  await app.listen(process.env.PORT ||3333)
+  // app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
+  await app.listen(process.env.PORT || 3333)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
--- recipe-app/src/recipe/recipe.module.ts
+++ recipe-app/src/recipe/recipe.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { RecipeResolvers } from './recipe.resolvers'
 import { RecipeService } from './recipe.service'
 import { PrismaModule } from '../prisma/prisma.module'
+import { S3Service } from 'src/s3/s3.service'
 
 @Module({
-  providers: [RecipeResolvers, RecipeService],
+  providers: [RecipeResolvers, RecipeService, S3Service],
   imports: [PrismaModule],
   exports: [RecipeService],
 })
