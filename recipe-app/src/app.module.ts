0a23c2fcdbd1c02a2f56f83179b3f48e3178a86c
Hsuanyin <61041593+hsuan81@users.noreply.github.com>
2023-02-27 02:32:24 +0800

    config to allow outside networks to request the grqphql

--- recipe-app/src/app.module.ts
+++ recipe-app/src/app.module.ts
@@ -28,6 +28,7 @@ import { AppConfigService } from './appconfig/appconfig.service'
       // sortSchema: true,
       debug: false,
       playground: false,
+      cors: true, // allow requests from outside networks
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     RecipeModule,
